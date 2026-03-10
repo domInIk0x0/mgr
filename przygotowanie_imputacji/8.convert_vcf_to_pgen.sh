69fccd8c881c2f4304155c624b8bf6f2d0b16636
domInIk0x0 <dominikpietrzak123p@gmail.com>
2026-03-10 11:51:43 +0100

    Add script to convert VCF to PGEN format

---
+++ przygotowanie_imputacji/8.convert_vcf_to_pgen.sh
@@ -0,0 +1,3 @@
+plink2 --vcf /mnt/ip114/DominikPietrzak/VCF_IMPUTATION_PREP/post_imputation_merged_vcf/all_chr_imp_merged_filtered_R2_075.vcf.gz \
+       --make-pgen 
+       --out /mnt/ip114/DominikPietrzak/PRS_ANALIZA/DANE_PO_IMPUTACJI_FILTROWANE_PO_JAKOSCI/all_chr_075
